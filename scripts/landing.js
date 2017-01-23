f29db3653cbfb76e6f05c373aa3e2991516ca350
Ted Bell <tchbell@gmail.com>
2017-01-23 14:36:44 -0500

    Completed checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
  
-var revealPoints = function (points) {
+var animatePoints = function (points) {
      
     for (var i=0; i<points.length; i++){
          points[i].style.opacity = 1;
@@ -11,3 +11,16 @@ var revealPoints = function (points) {
     
 };
 
+window.onload = function() {
+    if(window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+   window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+   });
+}
