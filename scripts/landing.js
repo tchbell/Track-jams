2a6704a2a9498c9085ea9dec89d09d59681e4333
Ted Bell <tchbell@gmail.com>
2017-01-23 21:17:06 -0500

    completed assignment-9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,30 @@
 var pointsArray = document.getElementsByClassName('point');
  
-var animatePoints = function (points) {
-     
-    for (var i=0; i<points.length; i++){
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
+var animatePoints = function (point) {
+      point.style.opacity = 1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
     
+    
+};
+
+var forEach = function (array, callback) {
+    for(var i=0; i<array.length; i++){
+    callback(array[i])
+    }
 };
 
 window.onload = function() {
     if(window.innerHeight > 950){
-        animatePoints(pointsArray);
+        forEach(pointsArray, animatePoint);
     }
      var sellingPoints = document.getElementsByClassName('selling-points')[0];
      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
    window.addEventListener('scroll', function(event){
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+             forEach(pointsArray, animatePoint);   
          }
    });
 }
---
+++ scripts/utilities.js
@@ -0,0 +1,14 @@
+var forEach = function (array, callback) {
+    for(var i=0; i<array.length; i++){
+    callback(array[i])
+    }
+};
+
+var animatePoint = function (point) {
+         point.style.opacity = 1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
+forEach(pointsArray, animatePoint);
