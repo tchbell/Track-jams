5a2cba9194c856e626af715cbad30f695f43ce1d
Ted Bell <tchbell@gmail.com>
2017-01-25 13:17:18 -0500

    Completed assignment-11-albumView

--- scripts/album.js
+++ scripts/album.js
@@ -1,12 +1,12 @@
 //Example Album
 
-var albumPicasso={
+var albumPicasso = {
     title: 'The Colors',
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
-    songs:[
+    songs: [
         {title: 'Blue', duration: '4:26' },
         {title: 'Green', duration: '3:14'},
         {title: 'Red', duration: '5:01' },
@@ -18,7 +18,7 @@ var albumPicasso={
 
 //Another example Album
 
-var albumMarconi={
+var albumMarconi = {
     title: 'The Telepone',
     artist: 'Guglielmo Marconi',
     label: 'EM',
@@ -29,43 +29,81 @@ var albumMarconi={
         {title: 'Ring, ring, ring', duration: '5:01' },
         {title: 'Fits in your pocket', duration: '3:21' },
         {title: 'Can you hear me now?', duration: '3:14' },
-        {title: 'Wrong phone number', duration: '2:15' },
+        {title: 'Wrong phone number', duration: '2:15' }
     ]
     
 };
 
-var createSongRow = function(songNumber, songName, songLength){
-    var template=
+var albumBlink = {
+    title: 'Take Off Your Pants And Jacket',
+    artist: 'Blink 182',
+    label: 'No Idea',
+    year: '1999',
+    albumArtUrl: 'assets/images/album_covers/03.png',
+    songs: [
+        {title: 'Your Mom', duration: '1:01' },
+        {title: 'Thats What she Said', duration: '5:01' },
+        {title: 'Oops I did it Again', duration: '3:21' },
+        {title: 'Shin Knockers?', duration: '3:14' },
+        {title: 'Not a Revolving Door', duration: '2:15' }
+    ]
+    
+};
+
+var createSongRow = function (songNumber, songName, songLength) {
+    var template =
         '<tr class="album-view-song-item">'
-    +   '   <td class="song-item-number>' +songNumber + '</td'
-    +   '   <td class="song-item-title">' + songName + '</td>'
-    +   '   <td class="song-item-duration">'  + songLength + '</td>'
-    +   '</tr>'
-    ;
+        +   '   <td class="song-item-number>' + songNumber + '</td'
+        +   '   <td class="song-item-title">' + songName + '</td>'
+        +   '   <td class="song-item-duration">'  + songLength + '</td>'
+        +   '</tr>';
     
     return template;
-}
-
-var setCurentAlbum = function(album){
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' +album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    
-    albumSongList.innerHTML = ' ';
-    
-    for (var i=0; i < album.songs.length; i++){
-        albumSongList.innerHTML += createSongRow(i+1, album.songs[i].duration);
-    }
 };
 
-window.onlaod = funciton(){
-    setCurrentAlbum(albumPicasso);
+ var setCurrentAlbum = function(album) {
+    
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+    
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     
+     albumSongList.innerHTML = '';
+ 
+    
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+ 
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+     
+var clickArt  = document.getElementsByTagName('img')[1]; 
+var albums =[albumPicasso, albumMarconi, albumBlink];
+var x =0;
+     
+     clickArt.addEventListener('click', function(event){
+         setCurrentAlbum(albums[x])
+         x++
+         if(x>2){
+             x=0;
+         }
+         })
+             
+         
+         
+         
+    
+
 };
+
+
