d07d2735184f08d2909881d081a950916c16e7bb
Ted Bell <tchbell@gmail.com>
2017-01-26 12:12:43 -0500

    finished checkpoint12

--- scripts/album.js
+++ scripts/album.js
@@ -37,14 +37,14 @@ var albumMarconi={
 var createSongRow = function(songNumber, songName, songLength){
     var template=
         '<tr class="album-view-song-item">'
-    +   '   <td class="song-item-number">' +songNumber + '</td>'
+    +   '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     +   '   <td class="song-item-title">' + songName + '</td>'
     +   '   <td class="song-item-duration">'  + songLength + '</td>'
     +   '</tr>'
     ;
     
     return template;
-}
+};
 
 var setCurrentAlbum = function(album){
     // #1
@@ -62,11 +62,12 @@ var setCurrentAlbum = function(album){
     albumSongList.innerHTML = ' ';
     
     for (var i=0; i < album.songs.length; i++){
-        albumSongList.innerHTML += createSongRow(i+1, album.songs[i].duration);
+        albumSongList.innerHTML += createSongRow(i+1, album.songs[i].title, album.songs[i].duration);
     }
 };
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
 
 //Album button templates
 
@@ -78,5 +79,11 @@ var songListContainer = document.getElementsByClassName('album-view-song-list')[
          if (event.target.parentElement.className === 'album-view-song-item') {
          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
-    })
-};
+    });
+     
+     for(var i=0; i<songRows.length; i++){
+         songRows[i].addEventListener('mouseleave', function(event){
+           this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');  
+         });
+     }
+}
