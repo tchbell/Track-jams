e4c5a696e88fc5b0d7d50fcd4e46f81a34ef7f2b
Ted Bell <tchbell@gmail.com>
2017-02-01 11:27:55 -0500

    Completed checkpoint 17

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,6 @@
-var collectionItemTemplate=
-    '<div class="collection-album-container column fourth">'
+var buildCollectionItemTemplate= function() {
+    var template = 
+        '<div class="collection-album-container column fourth">'
        +' <img src="assets/images/album_covers/01.png"/>'
        +' <div class="collection-album-info caption">'
         +   ' <p>'
@@ -14,12 +15,16 @@ var collectionItemTemplate=
    +' </div>'
 ;
 
-window.onload = function(){
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    return $(template);
+};
+
+
+$(window).load(function(){
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
     
-    collectionContainer.innherHTML= ' ';
-    
-    for (var i=0; i<12; i++){
-        collectionContainer.innerHTML+=collectionItemTemplate;
+    for(var i=0; i<12; i++){
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
